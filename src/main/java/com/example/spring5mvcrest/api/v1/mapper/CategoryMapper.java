a52b2024ee7e4a3918333d302a6154d5873c0c11
amadzia <agnieszkamadzia@onet.pl>
2018-08-30 09:05:43 +0200

    @Mapping - test of Intellij MapStruct plugin

--- src/main/java/com/example/spring5mvcrest/api/v1/mapper/CategoryMapper.java
+++ src/main/java/com/example/spring5mvcrest/api/v1/mapper/CategoryMapper.java
@@ -3,6 +3,7 @@ package com.example.spring5mvcrest.api.v1.mapper;
 import com.example.spring5mvcrest.api.v1.model.CategoryDTO;
 import com.example.spring5mvcrest.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,6 +11,7 @@ public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
+    @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 
 }
