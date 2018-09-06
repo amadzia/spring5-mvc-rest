1cc4073ff488f07356ddbf160838adf55a439c6c
amadzia <agnieszkamadzia@onet.pl>
2018-09-06 17:28:53 +0200

    Meta-data for Swagger changed

--- src/main/java/com/example/spring5mvcrest/config/SwaggerConfig.java
+++ src/main/java/com/example/spring5mvcrest/config/SwaggerConfig.java
@@ -3,13 +3,17 @@ package com.example.spring5mvcrest.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig { //extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
 
     @Bean
     public Docket api() {
@@ -17,16 +21,23 @@ public class SwaggerConfig { //extends WebMvcConfigurationSupport {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
 
-    //configuration used for non-Spring Boot apps
-//    @Override
-//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
+    private ApiInfo metaData() {
+
+        Contact contact = new Contact("Agnieszka", "https://example.com/about/", "a@example.com");
+
+        return new ApiInfo(
+                "Mvc rest",
+                "Spring Boot app",
+                "1.0",
+                "Terms of service",
+                contact,
+                "Some license",
+                "https://www.example.com/license",
+                new ArrayList<>(
+                ));
+    }
 }
